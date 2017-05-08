944e62e0c5524719a8ce4d8366fd442c41f22c4b
ChristofSeiler <christof.seiler@gmail.com>
2017-05-08 14:50:01 -0700

    added testing structure

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(CytoGLMM)
+
+test_check("CytoGLMM")
---
+++ tests/testthat/test_data_preparation.R
@@ -0,0 +1,6 @@
+library(CytoGLMM)
+context("data prepration")
+
+test_that("print is hello world", {
+  expect_match(hello(), "Hello, world!")
+})
