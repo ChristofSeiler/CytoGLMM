04ae3e9ecb9f3a723cf2d11344e70882c54f31b3
ChristofSeiler <christof.seiler@gmail.com>
2017-05-08 22:23:25 -0700

    adding plotting functions

---
+++ R/plot_donors.R
@@ -0,0 +1,21 @@
+#' Plot one density plot faceted by donors.
+#'
+#' @import ggplot2
+#' @export
+plot_donors <- function(df_samples,
+                       protein_name,
+                       histogram = TRUE) {
+  if(!histogram) {
+    ggplot(df_samples, aes_string(x = protein_name, color = "condition")) +
+      geom_density() +
+      facet_wrap(~ donor) +
+      ggtitle(protein_name) +
+      xlab("arcsinh transformed counts")
+  } else {
+    ggplot(df_samples, aes_string(x = protein_name, fill = "condition")) +
+      geom_histogram(binwidth=.2, alpha=.5, position="identity") +
+      facet_wrap(~ donor) +
+      ggtitle(protein_name) +
+      xlab("arcsinh transformed counts")
+  }
+}
---
+++ R/plot_markers.R
@@ -0,0 +1,20 @@
+#' Plot all markers for one donor.
+#'
+#' @import ggplot2
+#' @import reshape2
+#' @export
+plot_markers <- function(df_samples,
+                        donor_id,
+                        protein_names) {
+  df_samples_subset = subset(df_samples,donor == donor_id)
+  df_samples_subset = data.frame(df_samples_subset[,protein_names],
+                                 condition = df_samples_subset$condition)
+  df_samples_subset_long = melt(df_samples_subset,id = "condition")
+  print(
+    ggplot(df_samples_subset_long,aes(x = value,y = ..scaled..,color = condition)) +
+      geom_density() +
+      facet_wrap(~ variable,ncol = 8) +
+      ggtitle(donor_id) +
+      xlab("arcsinh transformed counts")
+  )
+}
