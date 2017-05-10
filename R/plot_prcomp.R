eeb5201a6d21374d25a45b8c8d5ea810af08c436
ChristofSeiler <christof.seiler@gmail.com>
2017-05-10 11:07:41 -0700

    subsample same number of cells for each donor in PCA

--- R/plot_prcomp.R
+++ R/plot_prcomp.R
@@ -9,9 +9,16 @@ plot_prcomp <- function(df_samples,
                         subsample_size = 10000,
                         seed = 1234) {
   set.seed(seed)
-  subsample_ids = sample(x = nrow(df_samples),
-                         size = subsample_size,
-                         replace = FALSE)
+  #subsample_ids = sample(x = nrow(df_samples),
+  #                       size = subsample_size,
+  #                       replace = FALSE)
+  cell_n = round(subsample_size/length(levels(df_samples$donor)))
+  cell_n_min = min(table(df_samples$donor))
+  if(cell_n_min < cell_n) cell_n = cell_n_min
+  subsample_ids = lapply(levels(df_samples$donor),function(donor_id) {
+    all_ids = which(df_samples$donor == donor_id)
+    sample(x = all_ids,size = cell_n)
+  }) %>% unlist
   df_samples_subset = df_samples[subsample_ids,]
   table(df_samples_subset$donor)
   res_pca = prcomp(df_samples_subset[,protein_names],scale. = FALSE)
