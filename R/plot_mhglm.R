6ebd0e0dc59643953c08cc4063ada39f915eb614
ChristofSeiler <christof.seiler@gmail.com>
2017-05-18 11:30:43 -0700

    simplified plotting of glmm and added order option

--- R/plot_mhglm.R
+++ R/plot_mhglm.R
@@ -2,7 +2,7 @@
 #'
 #' @export
 #'
-plot.mhglm <- function(res_glmm_ml) {
+plot.mhglm <- function(res_glmm_ml,order_abs = FALSE) {
   num_explanatory = nrow(summary(res_glmm_ml)$coefficient)-1
   adj_alpha = 0.05/num_explanatory
   ci = qnorm(1-adj_alpha/2)
@@ -10,9 +10,13 @@ plot.mhglm <- function(res_glmm_ml) {
   # add protein name
   coeff = data.frame(coeff,protein_name=rownames(coeff))
   # sort according to z-value
-  coeff$protein_name = factor(coeff$protein_name,
-                              levels = coeff$protein_name[order(coeff$z.value,
-                                                                decreasing = FALSE)])
+  levels_reordered = NULL
+  if(order_abs) {
+    levels_reordered = coeff$protein_name[order(abs(coeff$z.value),decreasing = FALSE)]
+  } else {
+    levels_reordered = coeff$protein_name[order(coeff$z.value,decreasing = FALSE)]
+  }
+  coeff$protein_name = factor(coeff$protein_name,levels = levels_reordered)
   coeff = data.frame(coeff,
                      high=coeff$Estimate+ci*coeff$Std..Error,
                      low=coeff$Estimate-ci*coeff$Std..Error)
@@ -27,13 +31,11 @@ plot.mhglm <- function(res_glmm_ml) {
     title = paste0("Prediction of ",
                    strsplit(as.character(res_glmm_ml$call)," ")[[2]][1])
   }
-  ggplot(coeff, aes(x=protein_name, y=Estimate)) +
-    geom_hline(yintercept = 0, col = "red") +
+  ggplot(coeff, aes(x=Estimate, y=protein_name)) +
+    geom_vline(xintercept = 0, col = "red") +
     geom_point() +
-    geom_segment(mapping=aes(x=protein_name, y=low, xend=protein_name, yend=high)) +
-    ylab(lab) +
+    geom_segment(mapping=aes(x=low, y=protein_name, xend=high, yend=protein_name)) +
+    xlab(lab) +
     labs(title = title) +
-    theme(plot.title = element_text(hjust = 0.5),
-          axis.title.y = element_blank()) +
-    coord_flip()
+    theme(axis.title.y = element_blank())
 }
