fd745e4bbb855529269aa2b4f1538a4fe53d8258
ChristofSeiler <christof.seiler@gmail.com>
2017-05-19 12:48:50 -0700

    using posterior expected FDR for graph DA

--- R/plot_pairwise_mi_da.R
+++ R/plot_pairwise_mi_da.R
@@ -10,11 +10,15 @@
 plot_pairwise_mi_da = function(fit,
                                df_samples,
                                protein_names) {
+
+  # for parallelizing over posterior samples
   registerDoParallel()
+
   # compute posterior median of pi and beta
   pi = rstan::extract(fit,pars = "pi")[[1]]
   beta = rstan::extract(fit,pars = "beta")[[1]]
-  # compare pairwise mutual informaiton between conditions
+
+  # compare pairwise mutual information between conditions
   x_list = list(c(1,0),c(1,1))
   M_da_list = foreach(i = 1:dim(pi)[1]) %dopar% {
     M_list = lapply(x_list,function(x) {
@@ -27,26 +31,42 @@ plot_pairwise_mi_da = function(fit,
     ifelse(M_list[[1]] > M_list[[2]],1,0)
   }
   M_da = Reduce('+', M_da_list)
-  # plot probabilities
-  M_probs = ifelse(test = M_da == 0,
-                   yes = 1/dim(pi)[1],
-                   no = M_da/dim(pi)[1])
-  title = paste0("P(",
-                 paste(rownames(contrasts(df_samples$condition)),
-                       collapse = " > "),
-                 " | data)")
+
+  # posterior expected FDR
+  # see Mitra, Mueller, and Ji (2016), Bayesian Graphical Models for Differential Pathways
+  fdr_threshold = function(FDR) {
+    P = M_da / dim(pi)[1]
+    seq_kappa = seq(0.5,1,1/(length(M_da_list)/10))
+    seq_kappa = kappa_seq[-length(seq_kappa)]
+    df_kappa = sapply(seq_kappa,function(kappa) {
+      ps = P[lower.tri(P)]
+      I = ifelse(test = ps>kappa,yes = 1,no = 0)
+      FDR_kappa = sum((1-ps)*I)/sum(I)
+      c(kappa=kappa,FDR_kappa=FDR_kappa)
+    }) %>% t %>% data.frame
+    kappa = df_kappa[which(df_kappa$FDR_kappa <= FDR)[1],"kappa"]
+    P_thres = ifelse(P > kappa,yes = FDR,no = NA)
+    P_thres
+  }
+  fdrs = c(0.1,0.01)
+  otherwise = paste0(">",fdrs[1])
+  fdr_list = lapply(fdrs,fdr_threshold)
+  M_fdr = do.call(pmin,c(fdr_list,na.rm = TRUE))
+  M_fdr[is.na(M_fdr)] = otherwise
+
+  # plot pairwise matrix
+  title = paste0(paste(rownames(contrasts(df_samples$condition)),
+                       collapse = " > ")," | data")
   get_upper_tri = function(cormat) {
     cormat[lower.tri(cormat)] = NA
-    diag(cormat) = 0 # doesn't give zero exactly
+    diag(cormat) = otherwise
     cormat
   }
-  M_long = melt(get_upper_tri(M_probs), na.rm = TRUE)
-  M_long = cbind(M_long,value_cut = cut(M_long$value,
-                                        breaks = c(0,0.95,0.99,1),
-                                        include.lowest = TRUE))
-  ggplot(data = M_long, aes(Var2, Var1, fill = value_cut)) +
+  M_long = melt(get_upper_tri(M_fdr), na.rm = TRUE)
+  M_long$value = factor(M_long$value,levels = c(otherwise,fdrs))
+  ggplot(data = M_long, aes(Var2, Var1, fill = value)) +
     geom_tile(color = "white") +
-    scale_fill_discrete(name = "Posterior\nprobability") +
+    scale_fill_brewer(name = "FDR",palette = "Greens") +
     theme_minimal() +
     theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1),
           axis.title.x=element_blank(),
