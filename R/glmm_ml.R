eb65cf148f7d3d1d69172d11f3effcf7d9fb5ca2
ChristofSeiler <christof.seiler@gmail.com>
2017-07-13 16:04:11 -0700

    added random_var argument

--- R/glmm_ml.R
+++ R/glmm_ml.R
@@ -7,19 +7,20 @@
 glmm_ml <- function(df_samples,
                     protein_names,
                     response,
+                    random_var = "donor",
                     cores = detectCores()) {
   registerDoParallel(cores = cores)
   markers_str = paste0(protein_names,collapse = " + ")
   formula_expr = NULL
   if( is.factor(df_samples[,response]) ) {
     formula_expr = parse(text = paste0("mhglm(",
-                                       paste(response,"~",markers_str,"+",paste0("(",markers_str," | donor),")),
+                                       paste(response,"~",markers_str,"+",paste0("(",markers_str," | ",random_var,"),")),
                                        "family = binomial(link='logit'),",
                                        "data = df_samples,",
                                        "control = mhglm.control(parallel = TRUE,fit.method = 'firthglm.fit'))"))
   } else {
     formula_expr = parse(text = paste0("mhglm(",
-                                       paste(response,"~",markers_str,"+",paste0("(",markers_str," | donor),")),
+                                       paste(response,"~",markers_str,"+",paste0("(",markers_str," | ",random_var,"),")),
                                        "family = gaussian(link = 'identity'),",
                                        "data = df_samples,",
                                        "control = mhglm.control(parallel = TRUE,fit.method = 'firthglm.fit'))"))
