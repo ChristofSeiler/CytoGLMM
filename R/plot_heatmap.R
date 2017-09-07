3b0e249397db7764af94369e17810f288100aca8
Christof Seiler <christof.seiler@gmail.com>
2017-09-06 18:03:35 -0700

    removed rownames in heatmap

--- R/plot_heatmap.R
+++ R/plot_heatmap.R
@@ -10,19 +10,18 @@ plot_heatmap = function(df_samples,
                         sample_info_names,
                         protein_names,
                         arrange_by_1,
-                        arrange_by_2 = "",
-                        rownames) {
+                        arrange_by_2 = "") {
     expr_median = df_samples %>%
       group_by(.dots = sample_info_names) %>%
       summarise_at(protein_names,median) %>%
       arrange_(arrange_by_1)
     if(nchar(arrange_by_2) > 0) expr_median %<>% arrange_(arrange_by_2)
     df_expr_median = as.data.frame(expr_median[,protein_names])
-    rownames(df_expr_median) = pull(expr_median,rownames)
+    rownames(df_expr_median) = 1:nrow(expr_median)
     col_names = arrange_by_1
     if(nchar(arrange_by_2) > 0) col_names = c(arrange_by_1,arrange_by_2)
     df_annotation = data.frame(expr_median[,col_names])
-    rownames(df_annotation) = pull(expr_median,rownames)
+    rownames(df_annotation) = 1:nrow(expr_median)
     color = colorRampPalette(brewer.pal(n = 9, name = "YlGnBu"))(100)
     pheatmap(t(df_expr_median),
              color = color,
