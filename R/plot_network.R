1b7243e69fce17b8b679f4ad8d449c2aac886391
Christof Seiler <christof.seiler@gmail.com>
2017-10-03 14:34:33 -0700

    plot empty network

--- R/plot_network.R
+++ R/plot_network.R
@@ -26,38 +26,41 @@ plot_network = function(tb_B,title_str) {
   nodes_set1 = tb_B_summary %>% dplyr::filter(max_median < 0)
   nodes_set2 = tb_B_summary %>% dplyr::filter(min_median > 0)
   nodes_all = list(nodes_set1,nodes_set2) %>% bind_rows
-  if(nrow(nodes_all) == 0)
-    stop("--- \n no significant nodes found \n  ---")
-
-  # make adjency matrices
-  A1 = matrix(data = 1,
-              nrow = nrow(nodes_set1),
-              ncol = nrow(nodes_set1))
-  A2 = matrix(data = 1,
-              nrow = nrow(nodes_set2),
-              ncol = nrow(nodes_set2))
-  A = bdiag(A1,A2) %>% as.matrix
-  A[A==0] = -1
-  dimnames(A) = list(nodes_all$protein_name,nodes_all$protein_name)
-
-  # make network
-  net = network(A,
-                directed = FALSE,
-                ignore.eval = FALSE,
-                names.eval = "correlation")
-
-  # ploting using ggnetwork
-  net_df = ggnetwork(net,layout = "circle")
-  net_df$correlation %<>% factor(levels = c(-1,1),
-                                 labels = c("negative", "positive"))
-  set.seed(0xdada2)
-  ggplot(net_df,aes(x = x,y = y,xend = xend,yend = yend)) +
-    geom_edges(aes(color = correlation),size = 1,alpha = 0.6) +
-    geom_nodes(size = 4) +
-    geom_nodelabel_repel(aes(label = vertex.names)) +
-    #scale_color_brewer(palette = "Dark2") +
-    scale_color_manual(values=c("#F8766D", "#00BFC4")) +
-    theme_blank() +
-    ggtitle(title_str) +
-    coord_fixed(ratio = 0.8)
+  
+  A = NULL
+  if(nrow(nodes_all) > 0) {
+    # make adjency matrices
+    A1 = matrix(data = 1,
+                nrow = nrow(nodes_set1),
+                ncol = nrow(nodes_set1))
+    A2 = matrix(data = 1,
+                nrow = nrow(nodes_set2),
+                ncol = nrow(nodes_set2))
+    A = bdiag(A1,A2) %>% as.matrix
+    A[A==0] = -1
+    dimnames(A) = list(nodes_all$protein_name,nodes_all$protein_name)
+    
+    # make network
+    net = network(A,
+                  directed = FALSE,
+                  ignore.eval = FALSE,
+                  names.eval = "correlation")
+    
+    # ploting using ggnetwork
+    net_df = ggnetwork(net,layout = "circle")
+    net_df$correlation %<>% factor(levels = c(-1,1),
+                                   labels = c("negative", "positive"))
+    set.seed(0xdada2)
+    ggplot(net_df,aes(x = x,y = y,xend = xend,yend = yend)) +
+      geom_edges(aes(color = correlation),size = 1,alpha = 0.6) +
+      geom_nodes(size = 4) +
+      geom_nodelabel_repel(aes(label = vertex.names)) +
+      #scale_color_brewer(palette = "Dark2") +
+      scale_color_manual(values=c("#F8766D", "#00BFC4")) +
+      theme_blank() +
+      ggtitle(title_str) +
+      coord_fixed(ratio = 0.8)
+  } else {
+    ggplot() + ggtitle("no significant nodes found")
+  }
 }
