b27de36fef98523963b22e8c7d66a27cb8e311e9
Christof Seiler <christof.seiler@gmail.com>
2017-11-02 20:50:45 -0700

    try different timing

--- R/cytomlogit.R
+++ R/cytomlogit.R
@@ -79,7 +79,7 @@ cytomlogit = function(df_samples_subset,
   reg = makeRegistry(file.dir = paste0("registry_",current_time),
                      packages = c("dplyr","magrittr","rstan"))
   reg$cluster.functions = makeClusterFunctionsSlurm(slurm_settings,
-                                                    scheduler.latency = 120,
+                                                    scheduler.latency = 1,
                                                     fs.latency = 120)
   batchMap(fun = run_vb,
            seed = seq(num_boot),
@@ -93,9 +93,8 @@ cytomlogit = function(df_samples_subset,
                               walltime = expected_walltime,
                               partition = partition,
                               measure.memory = TRUE),
-             sleep = 300,
              reg = reg)
-  waitForJobs(sleep = 300, reg = reg)
+  waitForJobs(expire.after = 120, reg = reg)
   if(!"1" %in% findDone()$job.id)
     stop("original bootstrap failed")
   model_fit_list = reduceResultsList(missing.val = NULL, reg = reg)
