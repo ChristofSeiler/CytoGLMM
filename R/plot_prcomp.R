1f8b3736f4a7b28aac2ecc06da88f77d361f55f6
Christof Seiler <christof.seiler@gmail.com>
2017-11-20 11:51:18 -0800

    improve PCA plotting

--- R/plot_prcomp.R
+++ R/plot_prcomp.R
@@ -18,19 +18,23 @@ plot_prcomp = function(dat,
   by_variable = dat %>% group_by_(color_var) %>% sample_n(n) %>% ungroup
   res_pca = prcomp(by_variable[,protein_names],scale. = FALSE)
   explained_var = (100*res_pca$sdev^2/sum(res_pca$sdev^2)) %>% round(.,1)
-  p1 = fviz_eig(res_pca,geom="bar")
+  xlab_str = paste0("PC1 (",explained_var[1],"%)")
+  ylab_str = paste0("PC2 (",explained_var[2],"%)")
+  p1 = fviz_eig(res_pca,geom="bar") + ylab("Variance in %")
   p2 = fviz_pca_var(res_pca, col.var="contrib",
                     gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
                     repel = repel) +
     coord_fixed(ratio = explained_var[2] / explained_var[1]) +
-    ggtitle("Markers")
+    ggtitle("Markers") +
+    xlab(xlab_str) +
+    ylab(ylab_str)
   by_variable %<>% add_column(PC1 = res_pca$x[,1])
   by_variable %<>% add_column(PC2 = res_pca$x[,2])
   p3 = ggplot(by_variable,aes(PC1,PC2)) +
     geom_density_2d(aes_string(col = color_var)) +
     coord_fixed(ratio = explained_var[2] / explained_var[1]) +
-    xlab(paste0("PC1 (",explained_var[1],"%)")) +
-    ylab(paste0("PC2 (",explained_var[2],"%)")) +
+    xlab(xlab_str) +
+    ylab(ylab_str) +
     ggtitle("Cells")
-  plot_grid(p1, p2, p3, nrow = 3)
+  plot_grid(p3, p1, p2, nrow = 2, rel_widths = c(0.7,0.3))
 }
