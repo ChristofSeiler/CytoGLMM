e72b881834c665b55b7368655f50926aff645b8e
Christof Seiler <christof.seiler@gmail.com>
2017-11-20 14:42:40 -0800

    generalize plot

--- R/plot_donors.R
+++ R/plot_donors.R
@@ -6,17 +6,18 @@
 plot_donors <- function(df_samples,
                        protein_name,
                        condition = "condition",
+                       group = "donor",
                        density = TRUE) {
   if(density) {
     ggplot(df_samples, aes_string(x = protein_name,y = "..scaled..",color = condition)) +
       geom_density() +
-      facet_wrap(~ donor) +
+      facet_wrap(group) +
       ggtitle(protein_name) +
       xlab("arcsinh transformed counts")
   } else {
     ggplot(df_samples, aes_string(x = protein_name,fill = condition)) +
       geom_histogram(binwidth=.2, alpha=.5, position="identity") +
-      facet_wrap(~ donor) +
+      facet_wrap(group) +
       ggtitle(protein_name) +
       xlab("arcsinh transformed counts")
   }
