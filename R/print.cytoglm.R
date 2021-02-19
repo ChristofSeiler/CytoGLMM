2a3a779ab6dfeab37b90c06839757e702dced7dd
Christof Seiler <christof.seiler@gmail.com>
2021-02-19 13:09:34 +0100

    fixing some last S3 warnings

--- R/print.cytoglm.R
+++ R/print.cytoglm.R
@@ -18,28 +18,28 @@
 #'                             group = "donor",
 #'                             num_boot = 10) # just for docs, in practice >=1000
 #' print(glm_fit)
-print.cytoglm = function(fit, ...) {
+print.cytoglm = function(x, ...) {
 
-  if(!is(fit, "cytoglm"))
+  if(!is(x, "cytoglm"))
     stop("Input needs to be a cytoglm object computed by cytoglm function.")
 
   cat("\n#######################\n")
-  if(fit$unpaired)  {
+  if(x$unpaired)  {
     cat("## unpaired anlaysis ##")
   } else {
     cat("## paired analysis ####")
   }
   cat("\n#######################\n\n")
-  cat("number of bootstrap samples:",fit$num_boot,"\n\n")
+  cat("number of bootstrap samples:",x$num_boot,"\n\n")
   cat("number of cells per group and condition:")
-  cell_count = table(pull(fit$df_samples_subset,fit$group),
-                     pull(fit$df_samples_subset,fit$condition))
+  cell_count = table(pull(x$df_samples_subset,x$group),
+                     pull(x$df_samples_subset,x$condition))
   print(cell_count)
 
-  cat("\nproteins included in the analysis:\n",fit$protein_names,"\n\n")
-  cat("condition compared:",fit$condition,"\n")
-  cat("grouping variable:",fit$group,"\n")
-  if(!is.null(fit$covariate_names))
-    cat("controlled covariates:",fit$covariate_names,"\n")
+  cat("\nproteins included in the analysis:\n",x$protein_names,"\n\n")
+  cat("condition compared:",x$condition,"\n")
+  cat("grouping variable:",x$group,"\n")
+  if(!is.null(x$covariate_names))
+    cat("controlled covariates:",x$covariate_names,"\n")
 
 }
