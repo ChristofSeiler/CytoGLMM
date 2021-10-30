e2d8be3f457937bb332c3a34ad691bb426e0604e
Christof Seiler <christof.seiler@gmail.com>
2021-10-30 11:33:12 +0200

    removed FlowRepositoryR
    the packages has been removed in the latest Bioconductor release

--- vignettes/precompile.R
+++
@@ -1,4 +0,0 @@
-# Precompiled vignettes that depend on long computations
-# Must manually move image files from CytoGLMM/figure to CytoGLMM/vignettes/figure after knit
-
-knitr::knit("vignettes/pregnancy_dataset.Rmd.orig", "vignettes/pregnancy_dataset.Rmd")
