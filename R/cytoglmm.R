06c1693e4e65aeada64800deead7d6bc2c1317e6
ChristofSeiler <christof.seiler@gmail.com>
2025-08-11 14:37:53 +0200

    Revert "removed mbest pkg"
    The package mbest was temporarily not available on CRAN. During that time we removed functions that dependent on mbest. Now mbest is back on CRAN. This commit reverts back to make all mbest-based functions available again.

--- R/cytoglmm.R
+++ R/cytoglmm.R
@@ -1,9 +1,7 @@
-#' Fit GLM with bootstrap resampling
+#' Fit GLMM with method of moments
 #'
 #' @import magrittr
 #' @import stringr
-#' @import BiocParallel
-#' @importFrom stats glm
 #' @export
 #'
 #' @param df_samples_subset Data frame or tibble with proteins counts,
@@ -15,11 +13,10 @@
 #' @param covariate_names The column names of covariates
 #' @param cell_n_min Remove samples that are below this cell counts threshold
 #' @param cell_n_subsample Subsample samples to have this maximum cell count
-#' @param num_boot Number of bootstrap samples
 #' @param num_cores Number of computing cores
 #'
 #' @return A list of class \code{cytoglm} containing
-#'   \item{tb_coef}{coefficent table}
+#'   \item{glmmfit}{\code{\link[mbest]{mbest}} object}
 #'   \item{df_samples_subset}{possibly subsampled df_samples_subset table}
 #'   \item{protein_names}{input protein names}
 #'   \item{condition}{input condition variable}
@@ -27,31 +24,30 @@
 #'   \item{covariate_names}{input covariates}
 #'   \item{cell_n_min}{input cell_n_min}
 #'   \item{cell_n_subsample}{input cell_n_subsample}
-#'   \item{unpaired}{true if unpaired samples were provided as input}
-#'   \item{num_boot}{input num_boot}
 #'   \item{num_cores}{input num_cores}
-#'   \item{formula_str}{formula use in the regression model}
 #'
 #' @examples
 #' set.seed(23)
 #' df <- generate_data()
 #' protein_names <- names(df)[3:12]
 #' df <- dplyr::mutate_at(df, protein_names, function(x) asinh(x/5))
-#' glm_fit <- CytoGLMM::cytoglm(df,
-#'                              protein_names = protein_names,
-#'                              condition = "condition",
-#'                              group = "donor",
-#'                              num_boot = 10) # in practice >=1000
-#' glm_fit
-cytoglm <- function(df_samples_subset,
-                    protein_names,
-                    condition,
-                    group = "donor",
-                    covariate_names = NULL,
-                    cell_n_min = Inf,
-                    cell_n_subsample = 0,
-                    num_boot = 100,
-                    num_cores = 1) {
+#' glmm_fit <- CytoGLMM::cytoglmm(df,
+#'                                protein_names = protein_names,
+#'                                condition = "condition",
+#'                                group = "donor")
+#' glmm_fit
+cytoglmm <- function(df_samples_subset,
+                     protein_names,
+                     condition,
+                     group = "donor",
+                     covariate_names = NULL,
+                     cell_n_min = Inf,
+                     cell_n_subsample = 0,
+                     num_cores = 1) {
+
+  # suppress printout of warning messages in R package mbest
+  logging::setLevel("ERROR", container = "mbest.mhglm")
+  logging::setLevel("ERROR", container = "mbest.mhglm.fit")
 
   # some error checks
   cyto_check(cell_n_subsample = cell_n_subsample,
@@ -80,39 +76,16 @@ cytoglm <- function(df_samples_subset,
       ungroup
   }
 
-  # formula
-  formula_str <- paste(condition,"~",
-                       paste(c(protein_names, covariate_names),
-                             collapse = " + "))
+  glmmfit <- glmm_moment(df_samples = df_samples_subset,
+                         protein_names = protein_names,
+                         response = condition,
+                         group = group,
+                         covariate_names = covariate_names,
+                         num_cores = num_cores)
 
-  # bootstrap
-  bs <- function(i) {
-    # bootstrap sample
-    df_boot <- df_samples_subset
-    df_boot %<>% group_by(.data[[ group ]], .data[[ condition ]])
-    df_boot %<>% slice_sample(prop = 1, replace = TRUE)
-    if(!unpaired) {
-      df_boot %<>% group_by(.data[[ group ]])
-    }
-    df_boot %<>%
-      group_split() %>%
-      sample(replace = TRUE) %>%
-      bind_rows()
-
-    # logistic regression
-    fit_glm <- glm(formula = formula_str,
-                   family = binomial(),
-                   data = df_boot)
-    tibble(protein_name = protein_names,
-           coeff = fit_glm$coefficients[protein_names],
-           run = i)
-  }
-  bpparam <- MulticoreParam(workers = num_cores)
-  tb_coef <- bplapply(seq_len(num_boot), bs, BPPARAM = bpparam) %>% bind_rows()
-
-  # return cytoglm object
+  # return cytoglmm object
   fit <- NULL
-  fit$tb_coef <- tb_coef
+  fit$glmmfit <- glmmfit
   fit$df_samples_subset <- df_samples_subset
   fit$protein_names <- protein_names
   fit$condition <- condition
@@ -120,11 +93,8 @@ cytoglm <- function(df_samples_subset,
   fit$covariate_names <- covariate_names
   fit$cell_n_min <- cell_n_min
   fit$cell_n_subsample <- cell_n_subsample
-  fit$unpaired <- unpaired
-  fit$num_boot <- num_boot
   fit$num_cores <- num_cores
-  fit$formula_str <- formula_str
-  class(fit) <- "cytoglm"
+  class(fit) <- "cytoglmm"
   fit
 
 }
---
+++ R/glmm_moment.R
@@ -0,0 +1,51 @@
+#' Generalized linear mixed model with maximum likelihood
+#'
+#' @import mbest
+#' @import doParallel
+#'
+#' @param df_samples Data frame or tibble with proteins counts,
+#'   cell condition, and group information
+#' @param protein_names A vector of column names of protein to use in the
+#'   analysis
+#' @param response The column name of the condition variable
+#' @param group The column name of the group variable
+#' @param covariate_names The column names of covariates
+#' @param num_cores Number of computing cores
+#'
+#' @return \code{\link[mbest]{mbest}} object
+#'
+glmm_moment <- function(df_samples,
+                        protein_names,
+                        response,
+                        group = "donor",
+                        covariate_names = NULL,
+                        num_cores = 1) {
+  parallel_str <- "FALSE"
+  if(num_cores > 1) {
+    registerDoParallel(cores = num_cores)
+    parallel_str <- "TRUE"
+  }
+  markers_str <- paste0(c(protein_names, covariate_names), collapse = " + ")
+  formula_expr <- NULL
+  if( is.factor(pull(df_samples,response)) ) {
+    formula_expr <- parse(
+      text = paste0(
+        "mhglm(",
+        paste(response,"~",markers_str,"+",
+              paste0("(",markers_str," | ",group,"), ")),
+        "family = binomial(link='logit'), ",
+        "data = df_samples, ",
+        "control = mhglm.control(parallel = ", parallel_str, ", ",
+        "fit.method = 'firthglm.fit'))"))
+  } else {
+    formula_expr <- parse(
+      text = paste0("mhglm(",
+                    paste(response,"~",markers_str,"+",
+                          paste0("(",markers_str," | ",group,"), ")),
+                    "family = gaussian(link = 'identity'), ",
+                    "data = df_samples, ",
+                    "control = mhglm.control(parallel = ", parallel_str, ", ",
+                    "fit.method = 'firthglm.fit'))"))
+  }
+  eval(formula_expr)
+}
